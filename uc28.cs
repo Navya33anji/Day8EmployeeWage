661e4babfb7343f129a213ce228f5fe49b3a8c90
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-09-28 18:30:10 +0530

    Employee Wage

---
+++ uc28.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Emp_Wage_UC2_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC2_Empwage.EmpWage();
+        }
+    }
+}
