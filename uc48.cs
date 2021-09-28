1617a53532685e1177de8f66278dd883a50234a8
NAVYATHA MEKALA <navyatha487@gmail.com>
2021-09-28 18:35:42 +0530

    Employee Rate per Hour

---
+++ uc48.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Emp_Wage_UC4_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmplyeeWageUsingSwitchCase.EmpWageUsingSwitch();
+        }
+    }
+}
